12f37ce8b64eea66e06a57bd39fd87f4cf5f1bca
Akshan <144763415+akkicasm06@users.noreply.github.com>
2024-04-30 14:49:45 +0530

    Update and rename QR.py to generate_qr_code.py

--- generate_qr_code.py
+++ generate_qr_code.py
@@ -1,6 +1,6 @@
 import qrcode
 
-def QR(data, filename):
+def generate_qr_code(data, filename):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -15,7 +15,7 @@ def QR(data, filename):
 
 if __name__ == "__main__":
     # Example data for QR code
-    data = "Akshan Khanna"
+        data = "AKSHAN KHANNA"
 
     # Generate QR code image
     generate_qr_code(data, "qrcode.png")
